bec3f3d22858487ba3ef561b68c326128949e608
Joy Chandra Mollik <xoycmollik913@gmail.com>
2021-12-05 14:15:28 +0600

    review section added

--- src/components/pages/Home/ReviewSection/ReviewSection.js
+++ src/components/pages/Home/ReviewSection/ReviewSection.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import SectionTitle from '../../../shared/SectionTitle/SectionTitle';
+import ReviewSectionCard from './ReviewSectionCard';
 
 const ReviewSection = () => {
 	return (
-		<div>
-			<h4>This is a reviewsection.</h4>
+		<div className='container mx-auto py-20'>
+			<SectionTitle title='loved by travellers' />
+			<div className='w-2/4 mx-auto'>
+				<ReviewSectionCard />
+			</div>
 		</div>
 	);
 };
---
+++ src/components/pages/Home/ReviewSection/ReviewSectionCard.js
@@ -0,0 +1,23 @@
+import { Avatar, Rating } from '@mui/material';
+import React from 'react';
+
+const ReviewSectionCard = () => {
+	return (
+		<div className='flex flex-col justify-center items-center space-y-4'>
+			<p className='text-para text-lg text-center'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+				magni consectetur iste accusantium atque corporis amet odio,
+				ullam id numquam, veniam ab earum quae laborum.
+			</p>
+			<Rating name='read-only' value={4} readOnly />
+			<h5 className=''>Kevin Kay</h5>
+			<Avatar
+				alt='Remy Sharp'
+				src='https://wp.getgolo.com/country-guide/wp-content/uploads/sites/6/2020/07/male-2.jpg'
+				sx={{ width: 56, height: 56 }}
+			/>
+		</div>
+	);
+};
+
+export default ReviewSectionCard;
