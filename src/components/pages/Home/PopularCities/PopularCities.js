d72639f36be795187f948134456817708bf23d1f
Joy Chandra Mollik <xoycmollik913@gmail.com>
2021-12-05 11:52:21 +0600

    added popular cities section

--- src/components/pages/Home/PopularCities/PopularCities.js
+++ src/components/pages/Home/PopularCities/PopularCities.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PopularCityCard from './PopularCityCard';
+
+const image =
+	'https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8UGFyaXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60';
 
 const PopularCities = () => {
 	return (
-		<div>
-			<h4>This is a popularcities.</h4>
+		<div className='bg-dark py-20'>
+			<div className='container mx-auto'>
+				{/* title */}
+				<div className='space-y-4 mb-10'>
+					<h4 className='text-2xl text-white font-semibold'>
+						Popular City
+					</h4>
+					<p
+						className='mx-auto bg-brand'
+						style={{ width: 70, height: 1 }}
+					/>
+				</div>
+				{/* content */}
+				<div className='flex justify-center items-center space-x-8'>
+					<PopularCityCard />
+					<PopularCityCard />
+					<PopularCityCard />
+					<PopularCityCard />
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/pages/Home/PopularCities/PopularCityCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PopularCityCard = () => {
+	return (
+		<article
+			style={{
+				height: 370,
+				width: 280,
+				background: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.25)), url(https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8UGFyaXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60) no-repeat center/cover`,
+			}}
+			className='w-full h-full text-white rounded-2xl overflow-hidden flex flex-col justify-between text-left p-7'
+		>
+			<h4 className='uppercase  top-5 left-5 font-bold text-sm'>
+				france
+			</h4>
+			<div className=' font-medium bottom-5 left-5 text-left'>
+				<h2 className='text-2xl'>Paris</h2>
+				<h5 className='text-base'>7 places</h5>
+			</div>
+		</article>
+	);
+};
+
+export default PopularCityCard;
